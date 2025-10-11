43e3974c64263981b0b7a6fe71541e16068cf9fa
Jakub Kuliński <jakubkuliinski@hotmail.com>
2025-10-11 12:48:14 +0200

    Added file with a skeleton for implementation of greedy algorithm

---
+++ alg_greedy.py
@@ -0,0 +1,15 @@
+#Algorytm Zachłanny
+
+# 1. Wczytywanie danych instancji
+
+# 2. Przydzielanie zadania na wolny procesor (zajętość procesora możemy liczyć po prostu zliczając czasy zadań do niego już przydzielonych)
+# 2.1 Procesory nich będą reprezentowane przez listę
+# 2.2 Zidentyfikować (jakoś) "wolny procesor" -> [najwolniejsza opcja] za każdym razem szukać minimum po liście procesorów i do niego dodawać
+# Jak sprytnie zapisywać, który procesor jest najszybciej wolny, tak, żeby to robić w czasie lepszym niż o(n^2)?
+# 2.3 Przypisać zadanie 
+
+# 3. Znaleźć, na którym procesorze jest najdłuższy czas
+
+# POMYSŁ: za pomocą odpowiedniego wyświetlania przedstawić uszeregowanie tych procesów
+
+

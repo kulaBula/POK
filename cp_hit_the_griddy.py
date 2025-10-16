7de7d8fab37a5a4e95360b0284eb5d6063620d69
oliwermiller <oliwer.miller@student.put.poznan.pl>
2025-10-16 21:10:08 +0200

    nwm

--- cp_hit_the_griddy.py
+++ cp_hit_the_griddy.py
@@ -1,6 +1,20 @@
 #Algorytm Zachłanny
 
+#funckja do wyświetlania uszeregowania
+def da_function(procesory):
+    print()
+    print("Procesor 1:", "_ " * procesory[0])
+    print("Procesor 2:", "_ " * procesory[1])
+    print("Procesor 3:", "_ " * procesory[2])
+    print("Procesor 4:", "_ " * procesory[3])
+    print("czas [s]   ", "_ " * (max(procesory)+1))
+    print("            ", end = "")
+    for i in range(1, max(procesory)+2): 
+        print(i, end = " ")
+    print()
+
 # 1. Wczytywanie danych instancji
+#f = open("cp_numbers.txt", "r")
 f = open("dane.txt", "r")
 lines = f.readlines()
 zadania = []
@@ -9,7 +23,7 @@ liczba_zadan = int(lines[1])
 for i in range(2, liczba_zadan+2):
     x = int(lines[i])
     zadania.append(x)
-print(zadania)
+print("Czasy zadań: ", zadania)
 # 2. Przydzielanie zadania na wolny procesor (zajętość procesora możemy liczyć po prostu zliczając czasy zadań do niego już przydzielonych)
 # 2.1 Procesory nich będą reprezentowane przez listę
 procesory = [0] * liczba_procesorow
@@ -18,9 +32,9 @@ procesory = [0] * liczba_procesorow
 for zadanie in zadania:
     ind_najwolniejszy_procesor = procesory.index(min(procesory))
     procesory[ind_najwolniejszy_procesor] += zadanie # 2.3 Przypisać zadanie
-print(procesory)
-
+print("Czasy procesorów: ", procesory)
+# POMYSŁ: za pomocą odpowiedniego wyświetlania przedstawić uszeregowanie tych procesów
+da_function(procesory)
 # 3. Znaleźć, na którym procesorze jest najdłuższy czas
 print("C_max = ", max(procesory))
-# POMYSŁ: za pomocą odpowiedniego wyświetlania przedstawić uszeregowanie tych procesów
     

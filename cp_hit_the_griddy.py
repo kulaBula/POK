48bd2f341a42407670baae137879a08933d2e59c
Jakub Kuliński <jakubkuliinski@hotmail.com>
2025-10-17 20:25:47 +0200

    Poprawiono komentarze i czytelnosc

--- cp_hit_the_griddy.py
+++ cp_hit_the_griddy.py
@@ -1,4 +1,4 @@
-#Algorytm Zachłanny
+#Algorytm Zachłanny problemu P||Cmax
 
 #funckja do wyświetlania uszeregowania
 def da_function(procesory):
@@ -12,6 +12,7 @@ def da_function(procesory):
     for i in range(1, max(procesory)+2): 
         print(i, end = " ")
     print()
+# Funkcja wizualizująca czasy uszeregowania zadań na procesorach bez formatowania
 def reprezentacja_kropkowa(procesory, liczba_procesorow, c_max):
     s = len(str(c_max)) - 2
     modul = 10**s
@@ -19,23 +20,23 @@ def reprezentacja_kropkowa(procesory, liczba_procesorow, c_max):
         print(f"Procesor {i+1}: ", "."*(int(procesory[i]/modul)), procesory[i])
 
 # 1. Wczytywanie danych instancji
-#f = open("cp_numbers.txt", "r")
-f = open("plik100.txt", "r")
+FILE = "plik100.txt" # TU PODAJ NAZWE PLIKU
+f = open(FILE, "r")
 lines = f.readlines()
 zadania = []
-wypis = []
+wypis = [] # Tablica potrzebna do wizualizacji zajętości procesorów
 liczba_procesorow = int(lines[0])
 liczba_zadan = int(lines[1])
+
 for i in range(2, liczba_zadan+2):
     x = int(lines[i])
     zadania.append(x)
-print("Czasy zadań: ", zadania)
-# 2. Przydzielanie zadania na wolny procesor (zajętość procesora możemy liczyć po prostu zliczając czasy zadań do niego już przydzielonych)
-# 2.1 Procesory nich będą reprezentowane przez listę
-procesory = [0] * liczba_procesorow
-
+#print("Czasy zadań: ", zadania)
+# 2. Przydzielanie zadania na wolny procesor
+procesory = [0] * liczba_procesorow # lista reprezentująca procesory
+# 2.1 Przydzielanie zadania do wolnego procesora
 for zadanie in zadania:
-    ind_najwolniejszy_procesor = procesory.index(min(procesory))
+    ind_najwolniejszy_procesor = procesory.index(min(procesory)) # wyszukanie wolnego procesora
     procesory[ind_najwolniejszy_procesor] += zadanie # 2.3 Przypisać zadanie
 #print("Czasy procesorów: ", procesory)
 
@@ -43,19 +44,22 @@ for zadanie in zadania:
 c_max = max(procesory)
 print("C_max = ", c_max)
 
-# Output z równymi przerwami
+# 4. Wizualizacja zajętości poszczególnych procesorów
 # Potrzebna jest lista procesor + nr procesora + czas + kropki z napisem
-col = [7, 3, 70]
+col = [7, 3, 70] # Szerokości kolumn
 cp_max = c_max
 s = len(str(cp_max))-2
 modul = 10**s
+# 4.1 Konstruowanie listy danych potrzebnej do formatowania
 for i in range(liczba_procesorow):
     wypis.append([])
     kropki = "."*int(procesory[i]/modul)+ " " + str(procesory[i])
     wypis[i].append(["Procesor", str(i+1), kropki])
 
-# Wypis przygotowanych danych
+# 4.2 Wypis przygotowanych danych
 for row in wypis:
     row= row[0]
     formatted = f"{row[0]:<{col[0]}} {row[1]:<{col[1]}} {row[2]:<{col[2]}}"
     print(formatted)
+
+
